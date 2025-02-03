28b9d17058aaf2494c63b3013f0599fa373cccbd
ayman.hmimdi@etu.univ-lyon1.fr <ayman.hmimdi@etu.univ-lyon1.fr>
2025-02-03 10:42:01 +0100

    Ajout de la page contact, Maj de la page d'accueil

--- script/script.js
+++ script/script.js
@@ -1,11 +1,8 @@
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+var typed = new Typed(".auto-typing", {
+    strings : ["Développeur FullStack", " Étudiant 2ème année en BUT Informatique"],
+    typeSpeed : 75,
+    backSpeed: 40,
+    loop: true
+})
 
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-}
+typed();
